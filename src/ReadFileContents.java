fa276af2770e84d8ab34c83a5ddfd4e9304f4f84
Elvina <elvina.smirnova.205@mail.ru>
2022-10-21 14:33:06 +0300

    создала новый класс считывающий файл

--- src/ReadFileContents.java
+++ src/ReadFileContents.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class ReadFileContents {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ReadFileContents {
+    public String readFileContentsOrNull(String path) {
+        try {
+            return Files.readString(Path.of(path));
+        } catch (IOException e) {
+            System.out.println("Невозможно прочитать файл с годовым отчётом. Возможно, файл не находится в нужной директории.");
+        }
+        return null;
+    }
 }
